6b7948ebc170660c18c6657604d1064419d016f0
km <kumamoto.yusuke@shanon.co.jp>
2024-08-05 12:14:58 +0900

    firstcommit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="Javascriptで漸化式を理解する" />
+    </div>
+  </header>
+  <nav>
+    <RouterLink to="/about">階乗（f(n)=n×(n-1)…×2×1）</RouterLink>
+  </nav>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
